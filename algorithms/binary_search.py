ab3720a13978af329d1f5e6dd6b70dce78497ef3
Krishna Babuji <krishna.pingal@gmail.com>
2024-08-22 21:45:07 -0500

    Add bisect_right to binary search

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -24,6 +24,16 @@ class Solution:
                 lo = mid + 1
         return A[lo]
 
+    def bisect_right(self, A: list[int], cond: Callable[[int], bool]) -> int:
+        lo, hi = 0, len(A) - 1
+        while lo < hi:
+            mid = (lo + hi + 1) // 2
+            if cond(A[mid]):
+                lo = mid
+            else:
+                hi = mid - 1
+        return A[hi]
+
 
 if __name__ == '__main__':
     sol = Solution()
@@ -32,3 +42,5 @@ if __name__ == '__main__':
     assert sol.bisect(A, 25) == -1
     assert sol.bisect_left(A, lambda x: x > 100) == 102
     assert sol.bisect_left(A, lambda x: x > 500) == 297
+    assert sol.bisect_right(A, lambda x: x < 100) == 99
+    assert sol.bisect_right(A, lambda x: x < -1) == 0
