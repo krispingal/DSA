8cedd51dcfdc67b55d231797ebb2a908bd958047
Krishna Babuji <krishna.pingal@gmail.com>
2024-09-03 01:42:27 -0500

    Add setuptools to project
    Added setuptools to make importing code across
    package simpler. Following best practices provided
    by setuptools for discovery involved in project restructuring.

--- src/dsa/algorithms/binary_search.py
+++ src/dsa/algorithms/binary_search.py
@@ -1,5 +1,5 @@
 from typing import Callable
-
+from src.dsa.utils.nums import get_sorted_nums
 
 class Solution:
     def bisect(self, A: list[int], target: int) -> int:
@@ -37,10 +37,10 @@ class Solution:
 
 if __name__ == '__main__':
     sol = Solution()
-    A = list(range(0, 300, 3))
-    assert sol.bisect(A, 27) == 9
-    assert sol.bisect(A, 25) == -1
-    assert sol.bisect_left(A, lambda x: x > 100) == 102
-    assert sol.bisect_left(A, lambda x: x > 500) == 297
-    assert sol.bisect_right(A, lambda x: x < 100) == 99
-    assert sol.bisect_right(A, lambda x: x < -1) == 0
+    A = get_sorted_nums()
+    assert sol.bisect(A, 46) == 8
+    assert sol.bisect(A, 50) == -1
+    # assert sol.bisect_left(A, lambda x: x > 100) == 102
+    # assert sol.bisect_left(A, lambda x: x > 500) == 297
+    # assert sol.bisect_right(A, lambda x: x < 100) == 99
+    # assert sol.bisect_right(A, lambda x: x < -1) == 0
